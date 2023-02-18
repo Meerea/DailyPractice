597037663664402b9a9518043bb214734336affc
Revathi Thiagarajan <rerag19@gmail.com>
2023-02-18 08:44:12 +0530

    Daily DSA

---
+++ src/test/java/TestCaseEnumeration/TestCase1.java
@@ -0,0 +1,46 @@
+package TestCaseEnumeration;
+/*
+public class TestCase1 {
+
+
+    /
+
+1.Amazon Search field
+
+Functional UI:
+1.Check the searched product loads in UI
+2.Check the suggestion loads while typing product name
+3.Check product loads with all details after once search complete
+
+API:
+1.Check the product list fetched on hitting the API - met time SLA
+2.Error code or message for invalid input parameters /URL
+
+Performance:
+1.Check search works as expected, in the increased user loads like in big billion days
+2.Check DB updates happens as expected
+
+Accessibility:
+1.Check able to search using keyboard keys
+2.Check UI font/colour accessible by all people
+3.Check voice search capable and fetched exact item
+
+
+
+2.GmailLogin:
+
+
+Functional UI:
+1.Valid username /valid password - Login successful
+2.Invalid username/valid password - error message
+3.Valid username/Invalid password - error message
+4.Invalid username/Invalid password - error message
+
+API:
+1.If more than  3 times tried with invalid username/password - send email to linked mail id stating activity (login) details
+2.Hit API and check response time
+3.API gets triggered for 2 factor login
+
+
+}
+*/
