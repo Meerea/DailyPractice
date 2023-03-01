82aa9da070aded175245da55310090fdd2c1087e
Revathi Thiagarajan <rerag19@gmail.com>
2023-03-01 19:21:37 +0530

    Daily assignment problems

---
+++ src/test/java/Assignments/ReverseWordsInSentenceWithAllSpace.java
@@ -0,0 +1,30 @@
+package Assignments;
+
+/*
+Given a string s, reverse the order of characters in each word within a sentence while still preserving
+whitespace and initial word order.
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+Input: s = "God Ding"
+Output: "doG gniD"
+
+TestData1 - s = "Let's take LeetCode contest"
+TestData2 - s="God Ding"
+TestData3- s="Goodday"
+
+Pseudocode:
+1.Declare String Array and add input sentence by split method using space and declare new string variable
+2.Iterate using for loop till the length of string array and convert each word to char[] array each time inside loop
+3.Declare left=0, right=char array length-1 and use while loop condition left less than right for char array
+4.Then swap the left & right chars of the char array and do left++,right--
+5.Add the reversed word
+5.Return the string
+
+ */
+
+public class ReverseWordsInSentenceWithAllSpace {
+
+
+}
